eed7a56a6a6c06d3cea867a16e6e315809454d48
fpaci2 <Dedalo$25>
2022-03-01 15:05:01 +0100

    tolto spring data jpa dal progetto

--- src/main/java/comboDev/arsdiapason/ArsdiapasonApplication.java
+++ src/main/java/comboDev/arsdiapason/ArsdiapasonApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @EnableTransactionManagement
-@EnableJpaRepositories
 @SpringBootApplication
 public class ArsdiapasonApplication {
 
