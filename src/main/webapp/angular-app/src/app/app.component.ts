da8825688eebc3798da7279c1b33cd1b40573c64
fpaci2 <Dedalo$25>
2022-03-02 20:12:38 +0100

    rinominata pagina Inserisci dati bambino

--- src/main/webapp/angular-app/src/app/app.component.ts
+++ src/main/webapp/angular-app/src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent {
         { title: 'Crea Account', route: '/createAccount', hidden: true },
         { title: 'Gestione Istituti', route: '/gestisciIstituti' },
         { title: 'Lista Bambini', route: '/childList' },
-        { title: 'Inserisci Dati Bambino', route: '/insertChild' },
+        { title: 'Inserisci Bambino', route: '/insertChild' },
         { title: 'Test Balconi', route: '/testBalconi' },
         { title: 'Risultato del Test', route: '/testResult', class: 'disabled' },
         { title: 'Grafico', route: '/paginaGrafici' }
