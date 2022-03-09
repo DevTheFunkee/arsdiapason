9fcacc1eccbdb0b421a82e3465c50d70d3267256
fpaci2 <Dedalo$25>
2022-03-09 19:46:44 +0100

    messi campi bambino non obbligatori in insert child

--- src/main/resources/myBatis/migrationConfig/scripts/20220302222600_add_field_test_finito_to_bambino.sql
+++ src/main/resources/myBatis/migrationConfig/scripts/20220302222600_add_field_test_finito_to_bambino.sql
@@ -1,9 +1,7 @@
 -- // add field test_finito to bambino
 
 ALTER TABLE bambino ADD test_finito TINYINT(1) NOT NULL DEFAULT 0;
-ALTER TABLE bambino ADD data_test DATE NOT NULL;
 
 -- //@UNDO
 
 ALTER TABLE bambino DROP COLUMN test_finito;
-ALTER TABLE bambino DROP COLUMN data_test;
