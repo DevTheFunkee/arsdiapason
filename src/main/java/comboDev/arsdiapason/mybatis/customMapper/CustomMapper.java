db35ef28c55a48bbada30fd66ef1556309d55e10
matteo <matteo.matteotti@gft.com>
2022-06-21 21:02:17 +0200

    add custom mapper

--- src/main/java/comboDev/arsdiapason/mybatis/customMapper/CustomMapper.java
+++ src/main/java/comboDev/arsdiapason/mybatis/customMapper/CustomMapper.java
@@ -11,4 +11,20 @@ public interface CustomMapper {
 
     @Select("SELECT DISTINCT sezione FROM bambino WHERE id_istituto = #{id}")
     List<String> findSezioni(@Param("id") Integer idIstituto);
+    
+    @Select({
+        "select",
+        "id, nome, regione, provincia, comune, indirizzo, mail",
+        "from istituto"
+    })
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+        @Result(column="nome", property="nome", jdbcType=JdbcType.VARCHAR),
+        @Result(column="regione", property="regione", jdbcType=JdbcType.VARCHAR),
+        @Result(column="provincia", property="provincia", jdbcType=JdbcType.VARCHAR),
+        @Result(column="comune", property="comune", jdbcType=JdbcType.VARCHAR),
+        @Result(column="indirizzo", property="indirizzo", jdbcType=JdbcType.VARCHAR),
+        @Result(column="mail", property="mail", jdbcType=JdbcType.VARCHAR)
+    })
+    List<Istituto> findAll();
 }
